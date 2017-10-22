c9a2ebd483668d95edd7c3c0cc7ef62fcb7f6ecf
Hugo Rivera <hugo.rivera2319@gmail.com>
2017-10-22 10:34:31 -0600

    Change title

--- app/App.js
+++ app/App.js
@@ -21,7 +21,7 @@ Navigation.registerComponent('comprpay.Nfc', () => AppLogin);
 Navigation.startSingleScreenApp({
     screen: {
         screen: 'comprpay.Login', // unique ID registered with Navigation.registerScreen
-        title: 'Login', // title of the screen as appears in the nav bar (optional)
+        title: 'ComprPay', // title of the screen as appears in the nav bar (optional)
         navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
         navigatorButtons: {} // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
     },
