732621f3eae903df856935b4c2d611aab57bac36
Ian Neal <wopr@wopr.tech>
2017-10-22 10:39:36 -0700

    more modo stuff but I don't think it's done yet but my computer is about to crash

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ console.log('Loading function');
 
 const doc = require('dynamodb-doc');
 const modo_pkg = require('modo');
+const modoCreds = {
+        key: 'uxSHXqjfPG9fGi3u5vpq1DKPAhjZS0_b',
+        secret: 'LschYY_dWBMAIUMK82u3-dBTtaaLdaWozwiAg9k4QOHFHC_ZzNF0m0oC5BiVEhjK', 
+};
 const modo = new modo_pkg.Modo({
         api_url: "api.sbx.gomo.do",
         base: "/api_v2",
-});
-
+}, modoCreds);
 
 var authentication = new MasterCardAPI.OAuth(consumerKey, keyStorePath, keyAlias, keyPassword);
 MasterCardAPI.init({
@@ -171,33 +174,54 @@ var getHosts = function(user, cb) {
         })
 }
 
-var createChargeCard = function(user, merchant, amount, hostCard, cb) {
+var createChargeCard = function(user, merchant, amount, providerCard, userCard, cb) {
         // User: {'name': string, 'modo_id': string}
         // Merchant: {'name': string, 'modo_id': string}
         // Amount: int (in pennies!)
-        // Host card: {'pan': string, 'exp_month': int, 'exp_year': int, 'zip': int, 'cvv': int}
-        // Open host card -- TODO only if we don't have it on file already!
-        let host_card_vault_id = modo.openCard(hostCard.pan, hostCard.exp_month, hostCard.exp_year, hostCard.zip, hostCard.cvv).response_data[0].vault_id;
+        // Provider card: {'pan': string, 'exp_month': int, 'exp_year': int, 'cvv': int, 'name': string}
+        // User card: {'pan': string, 'exp_month': int, 'exp_year': int, 'cvv': int, 'name': string}
+        // Open provider card -- TODO only if we don't have it on file already!
+        let provider_card_vault_id = openCard(providerCard.name, providerCard.pan, providerCard.exp_month, providerCard.exp_year, providerCard.cvv).response_data[0].vault_id;
+        let user_card_vault_id = openCard(userCard.name, userCard.pan, userCard.exp_month, userCard.exp_year, userCard.cvv).response_data[0].vault_id;
         let gc_params = {
                 'user_id': user.modo_id,
                 'amount': amount,
-                'description': 'mint OPEN_CARD to GIFT_CARD',
+                'description': 'mint OPEN_CARDx2 to GIFT_CARD',
                 'inputs': [
                         {
-                                'account_type': 'Card',
-                                'instrument_id': host_card_vault_id,
+                                'account_type': 'Card1',
+                                'instrument_id': provider_card_vault_id,
                                 'max_amount': amount
-                        }
+                        },
+                        {
+                                'account_type': 'Card2',
+                                'instrument_id': user_card_vault_id,
+                        },
                 ],
                 'outputs': [
                         {
                                 'account_type': 'GiftCard',
                                 'qualifier': '{"merchant_id": "' + merchant.modo_id + '"}',
-                                'max_amount': amount
                         }
                 ],
                 'auto_operate': true
         };
         let gift_card_vault_id = modo.query('/coin/mint', gc_params, null).response_data[0].vault_id;
 }
+
+var openCard = function(name, pan, exp_month, exp_year, cvv) {
+        return modo._getKey("OPEN_CARD").then((key) => {
+            return modo.query('/vault/add', {
+                'vault_type': 'OPEN_CARD',
+                'description': 'vault/add a json_to_be_encrypted credit card',
+                'json_to_be_encrypted': JSON.stringify({
+                    'pan': pan,
+                    'exp_month': exp_month,
+                    'exp_year': exp_year,
+                    'name': name,
+                    'cvv': cvv
+                })
+            }, null);
+        };
+    }
 
