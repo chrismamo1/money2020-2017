10928f1e4efaa0b7023d4f9432d15c961bdf9fc1
Hugo Rivera <hugo.rivera2319@gmail.com>
2017-10-22 12:25:59 -0600

    Add misplaced file

---
+++ app/getMerchantList.js
@@ -0,0 +1,15 @@
+function getMerchantList() {
+    // https://onzepay.localtunnel.me/login
+    // user=X&password=X
+    // 403
+
+    // list of merchants
+    return [ 
+        {key: "Starbucks"},
+        {key: "Papa Johns"},
+        {key: "Hot Topic"},
+        {key: "Radioshack"},
+    ]
+}
+
+export default getMerchantList;
